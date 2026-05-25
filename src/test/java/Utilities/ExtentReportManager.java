40454b287ed6b8cad97eb10f50e043f3329b2089
Vageesh <vageeshnpatil@gmail.com>
2026-05-25 18:04:59 +0530

    New Search page, Text case added

--- src/test/java/Utilities/ExtentReportManager.java
+++ src/test/java/Utilities/ExtentReportManager.java
@@ -93,13 +93,13 @@ public class ExtentReportManager implements ITestListener {
 		test.log(Status.FAIL,result.getName()+" got failed");
 		test.log(Status.INFO, result.getThrowable().getMessage());
 		
-		/*try {
+		try {
 			String imgPath = new BaseClass().captureScreen(result.getName());
 			test.addScreenCaptureFromPath(imgPath);
 			
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		}*/
+		}
 		
 	}
 
--- src/test/java/testCases/TC_004_SearchPage.java
+++ src/test/java/testCases/TC_004_SearchPage.java
@@ -1,10 +1,5 @@
 package testCases;
 
-import java.time.Duration;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
